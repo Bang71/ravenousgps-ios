604f887c9dacac906ed770b90a6f04c89f07870b
Bang71 <es.bang71@icloud.com>
2024-04-02 11:11:08 +0900

    conf: Create Xcode Project

---
+++ ravenousgps-ios/ravenousgps_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ravenousgps_iosApp.swift
+//  ravenousgps-ios
+//
+//  Created by 신병기 on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ravenousgps_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
